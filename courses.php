4e24fc3d545b498675e951a705efa6ca5dd5bec4
Ralf Ferreira <ralfferreira995@gmail.com>
2021-10-26 18:14:29 -0300

    Add svg images, Courses DONE

--- courses.php
+++ courses.php
@@ -25,48 +25,73 @@
                                     $x = $field['titulo_do_curso'];
                                     ?>
                                     <li class="splide__slide">
-                                        <div tabindex="1" class="course-item gallery-cell" onclick="opening(<?php echo $x ?>)">
+                                        <div tabindex="1" class="course-item gallery-cell" onclick="opening('<?php $new_str=str_replace(' ', '_', $x); echo $new_str?>')">
                                             <img class="course-item-icon" src="<?php echo $field['icone_do_curso']?>" alt="">
                                             <p><?php echo $field['titulo_do_curso']; ?></p>
                                         </div>
                                     </li>
-                                <?php }?>
+                                    <?php }?>
+                                    <script>
+
+                                    function closeAll(idd){
+                                            <?php
+                                                $close = CFS()->get( 'card_do_curso' );
+                                                foreach( $close as $target){
+                                                    $z = $target['titulo_do_curso'];
+                                                    $z =str_replace(' ', '_', $target['titulo_do_curso']);
+                                            ?>
+                                                if(<?php echo $z?> != idd){
+                                                    <?php echo $z?>.style.display = "none";
+                                                }
+                                            <?php }?>
+                                        }
+
+                                    function opening(id){
+                                        console.log(id)
+                                        var idd = document.getElementById(id);
+                                        console.log(idd)
+                                        closeAll(idd);
+                                        if(idd.style.display === "flex"){
+                                                idd.style.display="none";
+                                        } else{
+                                                idd.style.display="flex";
+                                        }
+                                    }
+                                    </script>
                         </ul>
                         </div>
                     </div>
+                    
                 <!-- fim mobile -->
                 <div class="courses-container">
+                    <script type="text/javascript">
+
+                    <?php 
+                        $fields = CFS()->get( 'conteudo' );
+                        foreach ( $fields as $field ) { ?>
+                            // console.log(a)
+                        <?php }?>
+                    // function closeAll(idd){
+                    //     if(a != idd){
+                    //         a.style.display="none";
+                    //     }
+                    // }
+                    // function opening(id){
+                    //     var idd = document.getElementById(id);
+                    //     closeAll(idd);
+                    //     if(idd.style.display === "flex"){
+                    //         idd.style.display="none";
+                    //     } else{
+                    //         idd.style.display="flex";
+                    //     }
+                    // }
+                    </script>
                     <?php
                         $fields = CFS()->get( 'conteudo' );
                         foreach ( $fields as $field ) { ?>
                         
-                        <script type="text/javascript">
-                        var y = "<?php echo $x?>";
-                        // function closeAll(idd){
-                        //     if(x != idd){
-                        //         x.style.display="none";
-                        //     }
-                        // }
 
-                        // function opening(<?php echo $field['titulo_do_curso'] ?>){
-                        //     var idd = document.getElementById(<?php echo $field['titulo_do_curso'] ?>);
-                        //     closeAll(idd);
-                        //     if(idd.style.display === "flex"){
-                        //         idd.style.display="none";
-                        //     } else{
-                        //         idd.style.display="flex";
-                        //     }
-                        // }
-                        function opening(y){
-                            var idd = y;
-                            if(idd.style.display === "flex"){
-                                idd.style.display="none";
-                            } else{
-                                idd.style.display="flex";
-                            }
-                        }
-                        </script>
-                    <div class="course-content" id="<?php echo $field['titulo_do_curso'] ?>">
+                    <div class="course-content" id="<?php $new_str=str_replace(' ', '_', $field['titulo_do_curso']); echo $new_str?>">
                         <div class="course-content-name">
                             <img src="<?php echo $field['icone_do_curso']?>" alt="">
                             <div>
